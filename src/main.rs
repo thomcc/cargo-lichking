a33d3dbcc8aa9ad9a70865f0997372b3239781f0
Wim Looman <wim@nemo157.com>
2018-01-17 11:09:33 +0100

    Update dependencies
    Requires raising minimum rustc version to support updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,22 @@ use options::{ Options, Cmd };
 fn main() {
     let matches = Options::app(false).get_matches();
     let options = Options::from_matches(&matches);
-    let config = Config::default().expect("No idea why this would fail");
-    let result = real_main(options, &config);
+    let mut config = Config::default().expect("No idea why this would fail");
+    let result = real_main(options, &mut config);
     if let Err(err) = result {
         config.shell().error(err).expect("Can't do much");
         process::exit(1);
     }
 }
 
-fn real_main(options: Options, config: &Config) -> CliResult {
+fn real_main(options: Options, config: &mut Config) -> CliResult {
     config.configure(
         options.verbose,
         Some(options.quiet),
         &options.color,
         options.frozen,
-        options.locked)?;
+        options.locked,
+        &[])?;
 
     config.shell().warn("IANAL: This is not legal advice and is not guaranteed to be correct.")?;
 
@@ -53,7 +54,7 @@ fn real_main(options: Options, config: &Config) -> CliResult {
         Cmd::List { by, package } => {
             let roots = load::resolve_roots(manifest_path.clone(), config, package)?;
             let packages = load::resolve_packages(manifest_path, config, &roots)?;
-            list::run(packages, config, by)?;
+            list::run(packages, by)?;
         }
     }
 
